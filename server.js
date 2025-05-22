e29a115333763d8ad7095adf98a1ca6a23617d1c
Kaveesha Sashen <sanaakaveesha@gmail.com>
2025-05-22 16:35:03 +0530

    installed cors ✏️`

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
-
+app.use(bodyParser.json());
+app.use(cors());
 mongoose
     .connect(process.env.DB_URI)
     .then(() => console.log('connected to db'))
     .catch(err => console.log(err));
-
 app.listen(port, () => {
     console.log(`server is running on port http://127.0.0.1:${port}`);
 });
