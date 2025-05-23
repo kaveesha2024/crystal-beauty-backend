cb2678727d951cc54a6573a8a88745ee5b04714b
Kaveesha Sashen <sanaakaveesha@gmail.com>
2025-05-23 12:35:29 +0530

    removed console logs in the code ✏️

--- actions/verify/GetOtpAction.js
+++ actions/verify/GetOtpAction.js
@@ -4,22 +4,16 @@ import Otp from "../../model/Otp.js";
 
 const GetOtpAction = async (request, response) => {
     const { email } = request.body;
-    console.log(email);
     if (email === "" || email === null) {
-        console.log("email is empty");
         response.json({
             status: 400,
             message: "Bad Request",
         });
         return;
     }
-    console.log("email is not empty");
     try {
-        console.log("trying to find user");
         const validUser = await User.findOne({ email: email });
-        console.log(validUser.firstName);
         if (!validUser) {
-            console.log("user not found");
             response.json({
                 status: 404,
                 message: "Email not found",
