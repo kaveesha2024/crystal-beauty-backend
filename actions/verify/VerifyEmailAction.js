f7546ede2b89d912bd7950ffc411b2a3f6d80cb0
Kaveesha Sashen <sanaakaveesha@gmail.com>
2025-05-29 12:16:18 +0530

    integrate email verification otp gateway ✏️

--- actions/verify/VerifyEmailAction.js
+++ actions/verify/VerifyEmailAction.js
@@ -1,2 +1,35 @@
-const VerifyEmailAction = async (request, response) => {};
+import { isBodyNull, isUserNull } from "../../utility/refactor/refactor.js";
+import Otp from "../../model/Otp.js";
+import User from "../../model/User.js";
+
+const VerifyEmailAction = async (request, response) => {
+    if (isUserNull(request.user) || isBodyNull(request.body)){
+        response.json({
+            status: 401,
+            message: "Unauthorized",
+        });
+        return;
+    }
+    try {
+        console.log(request.body.email, request.body.otp);
+        const validOtp = await Otp.findOne({email: request.body?.email});
+        if (validOtp && validOtp.otp == request.body?.otp) {
+            await User.updateOne({email: request.body?.email}, {isVerified: true});
+            response.json({
+                status: 200,
+                message: "Email verified",
+            });
+            return;
+        }
+        response.json({
+            status: 401,
+            message: "Your Otp is not valid and Email not verified successfully"
+        });
+    }catch (error) {
+        response.json({
+            status: 500,
+            message: "Internal Server Error",
+        });
+    }
+};
 export default VerifyEmailAction;
