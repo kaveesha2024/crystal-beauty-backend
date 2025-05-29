2f27c109a82dad5c50762895293161e0d5506929
Kaveesha Sashen <sanaakaveesha@gmail.com>
2025-05-29 14:48:43 +0530

    created product model ✏️

---
+++ model/Product.js
@@ -0,0 +1,60 @@
+import mongoose from 'mongoose';
+const productSchema = new mongoose.Schema({
+    productId: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    productName: {
+        type: String,
+        required: true,
+    },
+    labelledPrice: {
+        type: Number,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    discount:  Number,
+    stock: {
+        type: Number,
+        required: true,
+    },
+    warranty: {
+        type: String,
+        required: true,
+        default: "",
+    },
+    alterNames: [String],
+    description: {
+        type: String,
+        required: true,
+        maxLength: 1000,
+    },
+    brand: {
+        type: String,
+        required: true,
+    },
+    isAvailable: {
+        type: Boolean,
+        default: true,
+        required: true,
+    },
+    images: [String],
+    reviews: {
+        type: [{
+            comment: String,
+            reviewImages: [String],
+            isHidden: {
+                type: Boolean,
+                default: true,
+                required: true,
+            },
+        }],
+        default: [],
+    }
+}, { timestamps: true });
+const Product = mongoose.model('Products', productSchema);
+export default Product;
