617f38a42e025560d0a883ea6b9f3429ed0f7952
Kaveesha Sashen <sanaakaveesha@gmail.com>
2025-06-03 12:16:17 +0530

    Implement GetAllOrdersAction to fetch all orders with user permission checks ✏️

--- actions/Order/GetAllOrdersAction.js
+++ actions/Order/GetAllOrdersAction.js
@@ -1,4 +1,34 @@
+import Order from "../../model/Order.js";
+import { isAdmin, isUserNull } from "../../utility/reUsableFunctions/reUsableFunctions.js";
+
 const GetAllOrdersAction = async (request, response) => {
-    response.json(request.body);
+    if (isUserNull(request.user)||!isAdmin(request.user?.role)) {
+        response.json({
+            status: 403,
+            message: "Forbidden: You do not have permission to access this resource.",
+        });
+        return;
+    }
+    try {
+        const allOrders = await Order.find();
+        if (allOrders.length > 0) {
+            response.json({
+                status: 200,
+                message: allOrders
+            });
+            return;
+        }
+        else {
+            response.json({
+                status: 404,
+                message: "No orders found",
+            });
+        }
+    } catch (e) {
+        response.json({
+            status: 500,
+            message: "Internal Server Error",
+        });
+    }
 };
 export default GetAllOrdersAction;
